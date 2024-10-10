c609ec057859852b15e92be3356ccc8184ed3f3a
MatiasCirilloCH <124160152+MatiasCirilloCH@users.noreply.github.com>
2024-10-10 20:28:17 -0300

    Matias Cirillo

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,13 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
 async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +15,84 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+async function getSaleById(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .findOne({ _id: new ObjectId(id) });
+  return sale;
+}
+
+async function getSalesByStoreLocation(pageSize, page, storeLocation) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({ storeLocation: storeLocation })
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return sales;
+}
+
+async function getFilteredSales(pageSize, page, filter) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find(filter)
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return sales;
+}
+
+async function getTop10MostSellingProducts() {
+  const connectiondb = await getConnection();
+  const top10Products = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .aggregate([
+      { $unwind: "$items" },
+      {
+        $group: {
+          _id: "$items.name",
+          totalQuantity: { $sum: "$items.quantity" },
+        },
+      },
+      { $sort: { totalQuantity: -1 } },
+      { $limit: 10 },
+    ])
+    .toArray();
+
+  return top10Products.map(product => ({
+    name: product._id,
+    totalQuantity: product.totalQuantity
+  }));
+}
+
+async function getMostSatisfiedClients() {
+  const connectiondb = await getConnection();
+  const satisfiedClients = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .aggregate([
+      { $sort: { "customer.satisfaction": -1 } }
+    ])
+    .toArray();
+
+  return satisfiedClients.map(client => ({
+    clientEmail: client.customer.email,
+    clientSatisfaction: client.customer.satisfaction
+  }));
+}
+
+export { 
+  getAllSales, 
+  getSaleById, 
+  getSalesByStoreLocation, 
+  getFilteredSales, 
+  getTop10MostSellingProducts, 
+  getMostSatisfiedClients
+};
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { 
+  getAllSales,
+  getSaleById,
+  getSalesByStoreLocation,
+  getFilteredSales,
+  getTop10MostSellingProducts,
+  getMostSatisfiedClients
+} from "../data/sales.js";
 
 const router = express.Router();
 
@@ -9,5 +16,44 @@ router.get("/", async (req, res) => {
 
   res.json(await getAllSales(pageSize, page));
 });
+
+router.get("/id/:id", async (req, res) => {
+  const sale = await getSaleById(req.params.id);
+  res.json(sale);
+});
+
+router.get("/storeLocation/:storeLocation", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  res.json(await getSalesByStoreLocation(pageSize, page, req.params.storeLocation));
+});
+
+router.get("/filter", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const filter = {
+    storeLocation: req.query.storeLocation ? req.query.storeLocation : null,
+    purchaseMethod: req.query.purchaseMethod ? req.query.purchaseMethod : null,
+    couponUsed: (req.query.couponUsed === 'true')
+  }
+  console.log(filter);
+
+  res.json(await getFilteredSales(pageSize, page, filter));
+})
+
+router.get("/top10MostSellingProducts", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  res.json(await getTop10MostSellingProducts(pageSize, page));
+})
+
+router.get("/mostSatisfiedClient", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  res.json(await getMostSatisfiedClients(pageSize, page));
+})
 
 export default router;
