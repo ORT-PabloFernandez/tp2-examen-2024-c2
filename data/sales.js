fd38be70649f4624ac7873f82e8372a0dd41514a
dcozzani <damian.cozzani95@gmail.com>
2024-10-10 20:39:08 -0300

    Hasta punto 2

--- data/sales.js
+++ data/sales.js
@@ -1,4 +1,5 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
 const DATABASE = "sample_supplies";
 const MOVIES = "sales";
 
@@ -13,5 +14,27 @@ async function getAllSales(pageSize, page) {
     .toArray();
   return sales;
 }
+
+export async function getSalePorId(id){
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .findOne({ _id: new ObjectId(id)});
+
+  return sale;
+}
+
+export async function getSalesPorLocation(location){
+  const connectiondb = await getConnection();
+  const locationSales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({storeLocation: { $in: [location] } })
+    .toArray();
+  return locationSales
+}
+
+
 
 export { getAllSales };
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales , getSalesPorLocation , getSalePorId } from "../data/sales.js";
 
 const router = express.Router();
 
@@ -9,5 +9,31 @@ router.get("/", async (req, res) => {
 
   res.json(await getAllSales(pageSize, page));
 });
+
+router.get("/locationSales/:location", async (req, res) => {
+  try {   
+    const sales = await getSalesPorLocation(req.params.location);
+    res.json(sales);
+  } catch (error) {
+    res.status(500).send("Error obteniendo las ventas por locacion");
+  }
+
+})
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const sale = await getSalePorId(req.params.id);
+    if (sale) {
+      res.json(sale);
+    } else {
+      res.status(404).send("No se encontro la venta");
+    }
+  } catch (error) {
+    res.status(500).send("Error obteniendo la venta");
+  }
+});
+
+
 
 export default router;
