c787762fc78cc33f0aff2b15ca0d492666bc70ee
Parkizan <juanmartin.ibarbalz@gmail.com>
2024-10-10 20:44:47 -0300

    Juan Martin Ibarbalz

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,13 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
 async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +15,88 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+async function getSale(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .findOne({ _id: new ObjectId(id) });
+  return sale;
+}
+
+async function getSalesByLocation(location) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({ storeLocation: location })
+    .toArray();
+  return sales;
+}
+
+async function getSalesFiltered(storeLocation, purchaseMethod, couponUsed) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({
+      storeLocation: storeLocation,
+      purchaseMethod: purchaseMethod,
+      couponUsed: couponUsed === "true",
+    })
+    .toArray();
+  return sales;
+}
+
+async function getTopSellingProducts() {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .aggregate([
+      { $unwind: "$items" },
+      {
+        $group: {
+          _id: "$items.name",
+          totalQuantity: { $sum: "$items.quantity" },
+        },
+      },
+      {
+        $sort: { totalQuantity: -1 },
+      },
+      {
+        $limit: 10,
+      },
+    ])
+    .toArray();
+  return sales;
+}
+
+async function getMostSatisfiedClients() {
+  const connectiondb = await getConnection();
+  const clients = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .aggregate([
+      {
+        $group: {
+          _id: "$customer.email",
+          totalSatisfaction: { $sum: "$customer.satisfaction" },
+        },
+      },
+      {
+        $sort: { totalSatisfaction: -1 },
+      },
+    ])
+    .toArray();
+  return clients;
+}
+
+export {
+  getAllSales,
+  getSale,
+  getSalesByLocation,
+  getSalesFiltered,
+  getTopSellingProducts,
+  getMostSatisfiedClients,
+};
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import {
+  getAllSales,
+  getSale,
+  getSalesByLocation,
+  getSalesFiltered,
+  getTopSellingProducts,
+  getMostSatisfiedClients,
+} from "../data/sales.js";
 
 const router = express.Router();
 
@@ -8,6 +15,36 @@ router.get("/", async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllSales(pageSize, page));
+});
+
+router.get("/id/:id", async (req, res) => {
+  const sale = await getSale(req.params.id);
+  res.json(sale);
+});
+
+router.get("/location/:location", async (req, res) => {
+  const sales = await getSalesByLocation(req.params.location);
+  res.json(sales);
+});
+
+router.get("/filter", async (req, res) => {
+  const { storeLocation, purchaseMethod, couponUsed } = req.query;
+  const sales = await getSalesFiltered(
+    storeLocation,
+    purchaseMethod,
+    couponUsed
+  );
+  res.json(sales);
+});
+
+router.get("/topSellingProducts", async (req, res) => {
+  const sales = await getTopSellingProducts();
+  res.json(sales);
+});
+
+router.get("/mostSatisfied", async (req, res) => {
+  const sales = await getMostSatisfiedClients();
+  res.json(sales);
 });
 
 export default router;
