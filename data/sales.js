7b7f57eafdbc12b74095a71007055f321cb761a1
matthew <matthewmccreasteele123@gmail.com>
2024-10-10 21:46:41 -0300

    Matthew Martin McCrea Steele

--- data/sales.js
+++ data/sales.js
@@ -1,6 +1,7 @@
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
 const MOVIES = "sales";
+import {ObjectId} from "mongodb"
 
 async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
@@ -14,4 +15,50 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+async function getByFilters(location, purchaseMethod,couponUsed)
+{
+  console.log("Dentro de getByFilters");
+  const couponParse = (couponUsed === 'true');
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({
+      storeLocation: location,
+      purchaseMethod: purchaseMethod,
+      couponUsed: couponParse
+    })
+    .toArray();
+    console.log("Sales dentro del metodo:"+ sales);
+  return sales;
+
+}
+
+
+async function getSalesByLocation(location,pageSize, page)
+{
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+.db(DATABASE)
+.collection(MOVIES)
+.find({ storeLocation: location })
+.limit(pageSize)
+.skip(pageSize * page)
+.toArray();
+return sales;
+}
+
+
+
+async function getSaleById(id)
+{
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+.db(DATABASE)
+.collection(MOVIES)
+.findOne({_id:new ObjectId(id)});
+return sale;
+}
+
+
+export { getAllSales, getSaleById, getSalesByLocation,getByFilters};
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales,getSaleById,getSalesByLocation,getByFilters } from "../data/sales.js";
 
 const router = express.Router();
 
@@ -8,6 +8,68 @@ router.get("/", async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllSales(pageSize, page));
+});
+
+router.get("/topSellers", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  res.json(await getTopSellers(pageSize));
+});
+
+
+router.get("/filters/:location/:purchaseMethod/:couponUsed", async (req, res) => {
+  
+  const location = req.params.location;
+  const purchaseMethod = req.params.purchaseMethod;
+  const couponUsed = req.params.couponUsed;
+  
+  try {
+    console.log("Parametros:" + location,purchaseMethod,couponUsed);
+    const sales = await getByFilters(location,purchaseMethod,couponUsed);
+    console.log("sales"+sales);
+    return res.status(200).json(sales);
+
+  } 
+  catch (error) 
+  {
+    return res.status(500).json({message:"Error interno del servidor accediendo a filter", error:error.message});
+  }
+});
+
+
+router.get("/location/:location", async (req, res) => {
+  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+  const location = req.params.location
+
+  try {
+    const sales = await getSalesByLocation(location,pageSize,page);
+    return res.status(200).json(sales);
+
+  } 
+  catch (error) 
+  {
+    return res.status(500).json({message:"Error interno del servidor", error:error.message});
+  }
+});
+
+
+
+router.get("/:id", async (req, res) => {
+  
+const {id} = req.params;
+console.log("Id recibido por param "+id);
+
+try {
+  const sale = await getSaleById(id);
+  return res.status(200).json(sale);
+
+
+} catch (error) {
+  return res.status(500).json({message:"Error interno del servidor", error:error.message});
+}
+
 });
 
 export default router;
