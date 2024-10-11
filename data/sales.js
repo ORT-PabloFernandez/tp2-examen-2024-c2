9d53696550823d66f016daca7ab8f7a7c17e8258
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2024-10-10 21:10:07 -0300

    Ignacio Tobías García Godoy

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,13 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
-async function getAllSales(pageSize, page) {
+export async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +15,60 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+export async function getSaleByID(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .findOne({_id: new ObjectId(id)});
+  return sale;
+}
+
+export async function getSalesByStoreLocation(storeLocation, pageSize, page) {
+  const connectiondb = await getConnection();
+  const salesByStoreLocation = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({ storeLocation : storeLocation})
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return salesByStoreLocation;
+}
+
+export async function getSalesByFilters(storeLocation, purchaseMethod, couponUsed, pageSize, page) {
+  const connectiondb = await getConnection();
+  const query = {}
+
+  if(storeLocation){
+    query.storeLocation = storeLocation;
+  }
+
+  if(purchaseMethod){
+    query.purchaseMethod = purchaseMethod;
+  }
+
+  if(couponUsed){
+    query.couponUsed = couponUsed === 'true';
+  }
+  
+  const salesByFilters = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find(query)
+    .limit(pageSize)
+    .skip(pageSize * page)
+    .toArray();
+  return salesByFilters
+}
+
+export async function getCustomersByOrderOfSatisfaction() {
+  const connectiondb = await getConnection();
+  const customers = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({}, {projection: {customer:1}})
+    .sort({"customer.satisfaction": -1})
+    .toArray();
+  return customers;
+}
--- routes/sales.js
+++ routes/sales.js
@@ -1,12 +1,61 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales, getSaleByID, getSalesByStoreLocation, getSalesByFilters, getCustomersByOrderOfSatisfaction} from "../data/sales.js";
 
 const router = express.Router();
+
+router.get("/customersByOrderOfSatisfaction", async (req, res) => {
+  try{
+    const customers = await getCustomersByOrderOfSatisfaction()
+    res.json(customers)
+  } catch (error) {
+    res.status(500).json({"Error con el listado de clientes por oden de satisfaccion": error.message});
+  }
+});
+
+router.get("/filters-sales", async (req, res) => {
+  try{
+    const storeLocation = req.query.storeLocation;
+    const purchaseMethod = req.query.purchaseMethod;
+    const couponUsed = req.query.couponUsed;
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    
+    const sales = await getSalesByFilters(storeLocation, purchaseMethod, couponUsed, pageSize, page)
+    res.json(sales)
+  } catch (error) {
+    res.status(500).json({"Error en la filtracion de ventas": error.message})
+  }
+});
+
+router.get("/storeLocation/:store", async (req, res) => {
+  try{
+    const storeLocation = req.params.store;
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    const salesByStoreLocation = await getSalesByStoreLocation(storeLocation, pageSize, page);
+    res.json(salesByStoreLocation);
+  } catch (error) {
+    res.status(500).json({"Error en la obtencion de las ventas por la ubicacion de la tienda" : error.message});
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try{
+    const id = req.params.id;
+    const sale = await getSaleByID(id);
+    if(!sale){
+      res.status(404).json({message: "Error! No se encontro la venta"});
+    } else {
+      res.json(sale);
+    }
+  }catch (error) {
+    res.status(500).json({"Error en la obtencion de la venta con ID": error.message});
+  }
+});
 
 router.get("/", async (req, res) => {
   const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
   const page = req.query.page ? parseInt(req.query.page) : 0;
-
   res.json(await getAllSales(pageSize, page));
 });
 
