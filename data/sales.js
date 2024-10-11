2968461ca6c9a9017d8d6bdad910c5b34547df96
Malena Raiteri <mraiteri@renatre.org.ar>
2024-10-10 21:49:33 -0300

    Malena Raiteri

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,13 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
-async function getAllSales(pageSize, page) {
+export async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +15,47 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+export async function getSale(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .findOne({_id: new ObjectId(id)});
+  return sale;
+}
+
+export async function getSalesByStoreLocation(location) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({storeLocation: location})
+    .toArray();
+  return sales;
+}
+
+export async function getSalesByLocationPurchaseCoupon(location, method) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({storeLocation: location,
+      purchaseMethod: method,
+      couponUsed: true
+    })
+    .toArray();
+  return sales;
+}
+
+export async function getCostumerBySatisfaction() {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find()
+    .toArray();
+  const customers = sales
+    .map(sale => sale.customer)
+    .sort({"customer.satisfaction": -1});
+  return customers;
+}
--- routes/sales.js
+++ routes/sales.js
@@ -1,13 +1,67 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales, getSale, getSalesByStoreLocation, getSalesByLocationPurchaseCoupon, getCostumerBySatisfaction } from "../data/sales.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
-  const page = req.query.page ? parseInt(req.query.page) : 0;
-
-  res.json(await getAllSales(pageSize, page));
+  try {
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
+    const page = req.query.page ? parseInt(req.query.page) : 0;
+    res.json(await getAllSales(pageSize, page));  
+  } catch(error) {
+    console.log(error);
+    res.json(500).json({message: 'An error ocurred.'});
+  }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const saleId = req.params.id;
+    const sale = await getSale(saleId);
+    console.log(saleId);
+    if(sale) {
+      res.json(sale);
+    } else {
+      res.status(404).json({message: 'The sale does not exist.'})
+    }
+  } catch(error) {
+    console.log(error);
+    res.json(500).json({message: 'An error ocurred.'});
+  }
+});
+
+router.get("/location", async (req, res) => {
+  try {
+    const storeLocation = req.query.location;
+    const sale = await getSalesByStoreLocation(storeLocation);
+    res.json(sale);
+  } catch(error) {
+    console.log(error);
+    res.json(500).json({message: 'An error ocurred.'});
+  }
+});
+
+router.get("/location/method/coupon", async (req, res) => {
+  try {
+    const storeLocation = req.query.location;
+    const purchaseMethod = req.query.purchase;
+    const sale = await getSalesByLocationPurchaseCoupon(storeLocation, purchaseMethod);
+    res.json(sale);
+  } catch(error) {
+    console.log(error);
+    res.json(500).json({message: 'An error ocurred.'});
+  }
+});
+
+router.get("/satisfaction", async (req, res) => {
+  try {
+    const costumer = await getCostumerBySatisfaction();
+    res.json(costumer);
+  } catch(error) {
+    console.log(error);
+    res.json(500).json({message: 'An error ocurred.'});
+  }
+});
+
 
 export default router;
