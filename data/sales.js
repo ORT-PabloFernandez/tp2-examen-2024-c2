9f0ad9eae34e88258f0d4c882318b366cbbfa6f6
Sofia Laveihle <sofialaveihle@gmail.com>
2024-10-10 21:18:46 -0300

    Sofia Laveihle

--- data/sales.js
+++ data/sales.js
@@ -1,12 +1,14 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
+
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
 async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
-    .collection(MOVIES)
+    .collection(SALES)
     .find({})
     .limit(pageSize)
     .skip(pageSize * page)
@@ -14,4 +16,95 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+// 1
+// api/sales/5bd761dcae323e45a93cd0d3
+// Comentar para testear item 3 getSaleByFilters
+async function getOneSale(id) {
+  const connectiondb = await getConnection();
+
+  const sale = await connectiondb
+  .db(DATABASE)
+  .collection(SALES)
+  .findOne({ _id: new ObjectId(id)});
+
+  return sale;
+}
+
+// 2
+// api/sales/storeLocation/Seattle
+async function getSaleByStoreLocation(storeLocation) {
+  const connectiondb = await getConnection();
+
+  const filter = { storeLocation : storeLocation };
+
+  const sale = await connectiondb
+  .db(DATABASE)
+  .collection(SALES)
+  .find(filter)  
+  .toArray();
+
+  return sale;
+}
+
+// 3
+// api/sales/filter?storeLocation=[storeLocation]&purchaseMethod=[purchaseMetod]&couponUsed=[value]
+// api/sales/filterByStorePurchaseCoupon/filter?storeLocation=Seattle&purchaseMethod=Online&couponUsed=false
+async function getSaleByFilters(storeLocation, purchaseMethod, couponUsed) {
+  const connectiondb = await getConnection();
+
+  const saleFiltered = await connectiondb
+  .db(DATABASE)
+  .collection(SALES)
+  .find({
+    $and: [
+      { storeLocation: { $regex: new RegExp(storeLocation, 'i') }},
+      { purchaseMethod: { $regex: new RegExp(purchaseMethod, 'i') }},
+      { couponUsed: couponUsed === 'true'}
+    ]
+  })
+  .toArray();
+
+  return saleFiltered;
+}
+
+// 4
+// api/sales/products/topTenProducts
+async function getTopTenProducts() {
+  const connectiondb = await getConnection();
+
+  const topProducts = await connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .aggregate([ //Pipeline de agregación: agrupar valores de distintos documentos y devolver un solo resultado
+      //source: https://www.mongodb.com/docs/manual/aggregation/
+      { $unwind: "$items" }, //Descompone el array de items para hacer la suma de las cantidades
+      { 
+        $group: { //Agrupar items por: nombre de producto y el total de cantidades vendidas
+          _id: "$items.name",
+          totalSold: { $sum: "$items.quantity" } //Suma la cantidad vendida de cada item
+        } 
+      },
+      { $sort: { totalSold: -1 } }, //Ordenado de manera descendente
+      { $limit: 10 } //Devuelve solo 10 objetos
+    ])
+    .toArray();
+
+  return topProducts;
+}
+
+// 5
+// api/sales/customers/satisfaction
+async function getCustomersBySatisfaction() {
+  const connectiondb = await getConnection();
+
+  const customers = await connectiondb
+  .db(DATABASE)
+  .collection(SALES)
+  .find({ "customer.satisfaction": { $exists: true }})
+  .sort({ "customer.satisfaction": -1}) //Ordenado de manera descendente
+  .toArray();
+
+  return customers;
+}
+
+export { getOneSale, getAllSales, getSaleByStoreLocation, getSaleByFilters, getCustomersBySatisfaction, getTopTenProducts };
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getOneSale, getAllSales, getSaleByStoreLocation, getSaleByFilters, getCustomersBySatisfaction, getTopTenProducts} from "../data/sales.js";
 
 const router = express.Router();
 
@@ -8,6 +8,54 @@ router.get("/", async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllSales(pageSize, page));
+});
+
+// 1
+// api/sales/5bd761dcae323e45a93cd0d3
+router.get("/:id", async(req, res) => {
+  try {
+    const sale = res.json(await getOneSale(req.params.id));
+    res.json(sale);
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error'});
+  } 
+});
+
+// 2
+// api/sales/storeLocation/Seattle
+router.get("/storeLocation/:storeLocation", async(req, res) => {
+  const { storeLocation } = req.params;
+
+  try {
+    const saleByStoreLocation = res.json(await getSaleByStoreLocation(storeLocation));
+    res.json(saleByStoreLocation);
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error'});
+  } 
+});
+
+// 3
+// api/sales/filter?storeLocation=[storeLocation]&purchaseMethod=[purchaseMetod]&couponUsed=[value]
+// api/sales/filterByStorePurchaseCoupon/filter?storeLocation=Seattle&purchaseMethod=Online&couponUsed=false
+router.get("/filterByStorePurchaseCoupon/filter", async (req, res) => {
+  const { storeLocation, purchaseMethod, couponUsed } = req.query;
+
+  res.json(await getSaleByFilters(storeLocation, purchaseMethod, couponUsed));
+})
+
+// 4 
+// api/sales/products/topTenProducts
+router.get("/products/topTenProducts", async (req, res) => {
+  res.json(await getTopTenProducts());
+})
+
+// 5
+// api/sales/customers/satisfaction
+router.get("/customers/satisfaction", async (req, res) => {
+  const customers = await getCustomersBySatisfaction();
+  res.json(customers);
 });
 
 export default router;
