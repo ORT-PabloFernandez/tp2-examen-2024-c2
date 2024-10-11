9c6e82d0ec1f26c9049c257c143e1fd275865d5f
Valentino <valentienemail@gmail.com>
2024-10-10 21:30:21 -0300

    Valentino Caceres

--- data/sales.js
+++ data/sales.js
@@ -1,8 +1,9 @@
+import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
 const DATABASE = "sample_supplies";
 const MOVIES = "sales";
 
-async function getAllSales(pageSize, page) {
+export async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
@@ -14,4 +15,53 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+export async function getSale(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .findOne({ _id: new ObjectId(id) });
+  if (!sale) {
+    throw new Error("sale no encontrada");
+  }
+  return sale;
+}
+
+export async function getSalesByLocations(location) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({ storeLocation: location })
+    .toArray();
+
+  return sales;
+}
+
+export async function getSalesExSales(locacion, metodo, cupon) {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({
+      storeLocation: locacion,
+      purchaseMethod: metodo,
+      couponUsed: cupon,
+    })
+    .toArray();
+
+  return sales;
+}
+
+export async function getTenMostSold() {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({}, { proyection: { "items.quantity": 1, _id: 0 } })
+    .sort({ "items.quantity": -1 })
+    .limit(10)
+    .toArray();
+
+  return sales;
+}
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import {
+  getAllSales,
+  getSale,
+  getSalesByLocations,
+  getSalesExSales,
+  getTenMostSold,
+} from "../data/sales.js";
+
+// no pude practicar, no dejaste el link del server de MONGODB para el simulacro
 
 const router = express.Router();
 
@@ -8,6 +16,30 @@ router.get("/", async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllSales(pageSize, page));
+});
+
+router.get("/:id", async (req, res) => {
+  const sale = await getSale(req.params.id);
+  res.json(sale);
+});
+
+router.post("/", async (req, res) => {
+  const sales = await getSalesByLocations(req.body.storeLocation);
+  res.json(sales);
+});
+
+router.post("/", async (req, res) => {
+  const sales = await getSalesExSales(
+    req.body.storeLocation,
+    req.body.purchaseMethod,
+    req.body.couponUsed
+  );
+  res.json(sales);
+});
+
+router.get("/", async (req, res) => {
+  const sales = await getTenMostSold();
+  res.json(sales);
 });
 
 export default router;
