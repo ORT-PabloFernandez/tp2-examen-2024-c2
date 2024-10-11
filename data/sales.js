9e21176f83771bfa3f557bc5cdd26a42266d582b
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-10-10 21:06:23 -0300

    Paula Alvarez

--- data/sales.js
+++ data/sales.js
@@ -1,8 +1,9 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
 const DATABASE = "sample_supplies";
 const MOVIES = "sales";
 
-async function getAllSales(pageSize, page) {
+export async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
@@ -14,4 +15,86 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+export async function getSaleById(id) {
+  const connectiondb = await getConnection();
+  const sale = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .findOne({ _id: new ObjectId(id) });
+
+  return sale;
+}
+
+
+export async function getSalesByLocation(storeLocation){
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find( {storeLocation : storeLocation} )
+    .toArray();
+  
+    return sales;
+}
+
+export async function getSalesFiltered(storeLocation, purchaseMethod, couponUsed){
+  const connectiondb = await getConnection();
+  const salesFiltered = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find( {
+      $and: [
+      { storeLocation: { $regex: new RegExp(storeLocation, 'i') }},
+			{ purchaseMethod: { $regex: new RegExp(purchaseMethod, 'i') }},
+			{ couponUsed: couponUsed === 'true' }
+      ]
+    })
+    .toArray();
+  
+    return salesFiltered;
+}
+
+export async function getTopSoldProducts() {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .toArray();
+  
+  const productQuantities = {};
+
+  sales.forEach(sale => {
+    sale.items.forEach(item => {
+      if (productQuantities[item.name]) {
+        productQuantities[item.name] += item.quantity;
+      } else {
+        productQuantities[item.name] = item.quantity;
+      }
+    });
+  });
+
+  const topProducts = Object.keys(productQuantities)
+    .map(name => ({ name, totalQuantity: productQuantities[name] }))
+    .sort((a, b) => b.totalQuantity - a.totalQuantity)
+    .slice(0, 10);
+
+  return topProducts;
+}
+
+
+export async function getClientsSatisfaction() {
+  const connectiondb = await getConnection();
+  const sales = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .toArray();
+  const clients = sales
+    .map((sale) => ({
+      client: sale.customer,
+      satisfaction: sale.satisfaction,
+    }))
+    .sort((a, b) => b.client.satisfaction - a.client.satisfaction);
+  return clients;
+}
--- routes/sales.js
+++ routes/sales.js
@@ -1,7 +1,36 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import { getAllSales, getSaleById, getSalesByLocation, getSalesFiltered, getTopSoldProducts, getClientsSatisfaction } from "../data/sales.js";
 
 const router = express.Router();
+
+// TEST - ENDPOINT http://localhost:3000/api/sales/5bd761dcae323e45a93cd02b
+router.get("/:id", async (req, res) => {
+  res.json(await getSaleById(req.params.id));
+});
+
+
+// TEST - ENDPOINT http://localhost:3000/api/sales/location/Austin
+router.get("/location/:storeLocation", async(req,res) => {
+  res.json(await getSalesByLocation(req.params.storeLocation))
+})
+
+
+// TEST - ENDPOINT http://localhost:3000/api/sales/filterSales/filter?storeLocation=denver&purchaseMethod=In%20store&couponUsed=true
+router.get("/filterSales/filter", async (req,res) => {
+    const { storeLocation, purchaseMethod, couponUsed } = req.query;
+    res.json(await getSalesFiltered(storeLocation, purchaseMethod, couponUsed))
+})
+
+
+// TEST - ENDPOINT http://localhost:3000/api/sales/products/top-products
+router.get("/products/top-products", async (req,res) => {
+  res.json(await getTopSoldProducts());
+})
+
+// TEST - ENDPOINT http://localhost:3000/api/sales/clients/clientsatisfaction
+router.get("/clients/clientsatisfaction", async (req, res) => {
+  res.json(await getClientsSatisfaction());
+});
 
 router.get("/", async (req, res) => {
   const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 0;
