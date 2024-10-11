fb55fe336489d57addb78f0698159fac6e886fe9
Manuel Branca <manubranca0227@gmail.com>
2024-10-10 21:18:55 -0300

    Manuel Branca

--- data/sales.js
+++ data/sales.js
@@ -1,8 +1,9 @@
 import getConnection from "./conn.js";
+import { ObjectId } from "mongodb";
 const DATABASE = "sample_supplies";
-const MOVIES = "sales";
+const SALES = "sales";
 
-async function getAllSales(pageSize, page) {
+export async function getAllSales(pageSize, page) {
   const connectiondb = await getConnection();
   const sales = await connectiondb
     .db(DATABASE)
@@ -14,4 +15,107 @@ async function getAllSales(pageSize, page) {
   return sales;
 }
 
-export { getAllSales };
+export async function getSaleById(id) {
+  const connectiondb = await getConnection();
+  const Sale = connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .findOne({ _id: new ObjectId(id) });
+  return Sale;
+}
+
+export async function getSaleByLocation(location) {
+  const connectiondb = await getConnection();
+  const Sales = connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({ storeLocation: location })
+    .toArray();
+  return Sales;
+}
+
+export async function getAdvancedFilter(location, purchaseMethod) {
+  const connectiondb = await getConnection();
+  const Sales = connectiondb
+    .db(DATABASE)
+    .collection(SALES)
+    .find({
+      storeLocation: location,
+      purchaseMethod: purchaseMethod,
+      couponUsed: true,
+    })
+    .toArray();
+  return Sales;
+}
+
+export async function getTopTen() {
+  const connectiondb = await getConnection();
+  const salesCollection = connectiondb.db(DATABASE).collection(SALES);
+
+  const sales = await salesCollection.find().toArray();
+
+  const productSalesCount = {};
+
+  sales.forEach((sale) => {
+    const soldProducts = sale.items;
+    soldProducts.forEach((object) => {
+      const objectName = object.name;
+      const quantity = object.quantity;
+
+      if (productSalesCount[objectName]) {
+        productSalesCount[objectName] += quantity;
+      } else {
+        productSalesCount[objectName] = quantity;
+      }
+    });
+  });
+
+  const orderProducts = [];
+
+  for (const objectName in productSalesCount) {
+    orderProducts.push({
+      name: objectName,
+      quantity: productSalesCount[objectName],
+    });
+  }
+
+  orderProducts.sort((a, b) => b.quantity - a.quantity);
+
+  const topTen = orderProducts.slice(0, 10);
+
+  return topTen;
+}
+
+export async function getClientsBySatisfaction() {
+  const connectiondb = await getConnection();
+  const salesCollection = await connectiondb.db(DATABASE).collection(SALES);
+
+  const sales = await salesCollection.find().toArray();
+
+  const customersMap = {};
+
+  sales.forEach((sale) => {
+    const customer = sale.customer;
+
+    if (customer && customer.satisfaction !== undefined) {
+      const customerEmail = customer.email;
+
+      if (!customersMap[customerEmail]) {
+        customersMap[customerEmail] = {
+          email: customer.email,
+          satisfaction: customer.satisfaction,
+        };
+      }
+    }
+  });
+
+  const customersArray = [];
+
+  for (const email in customersMap){
+    customersArray.push(customersMap[email])
+  }
+
+  customersArray.sort((a,b) => b.satisfaction - a.satisfaction)
+
+  return customersArray;
+}
--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getAllSales } from "../data/sales.js";
+import {
+  getAdvancedFilter,
+  getAllSales,
+  getClientsBySatisfaction,
+  getSaleById,
+  getSaleByLocation,
+  getTopTen,
+} from "../data/sales.js";
 
 const router = express.Router();
 
@@ -8,6 +15,38 @@ router.get("/", async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 0;
 
   res.json(await getAllSales(pageSize, page));
+});
+
+router.get("/bylocation/:location", async (req, res) => {
+  console.log("Sales By Location");
+  const sales = await getSaleByLocation(req.params.location);
+  res.json(sales);
+});
+
+router.get("/advancedfilter", async (req, res) => {
+  console.log("Advanced Filter");
+  const { location, purchaseMethod } = req.query;
+  console.log(location, purchaseMethod);
+  const sales = await getAdvancedFilter(location, purchaseMethod);
+  res.json(sales);
+});
+
+router.get("/topten", async (req, res) => {
+  console.log("Top ten");
+  const Topten = await getTopTen();
+  res.json(Topten);
+});
+
+router.get("/clients/satisfactionlist", async(req,res) => {
+  console.log("Satisfaction List")
+  const List = await getClientsBySatisfaction()
+  res.json(List)
+})
+
+router.get("/:id", async (req, res) => {
+  console.log("ID");
+  const sale = await getSaleById(req.params.id);
+  res.json(sale);
 });
 
 export default router;
